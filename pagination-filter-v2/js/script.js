dac87643234688c2d0b2d0d16713056b84413188
Ian Ross <ianhowesross@gmail.com>
2017-09-13 17:03:20 -0400

    WIP showPage function

--- pagination-filter-v2/js/script.js
+++ pagination-filter-v2/js/script.js
@@ -1,1 +1,22 @@
-console.log("Testing");
+
+const studentList = $(".student-item");
+const numOfStudents = studentList.length;
+const maxStudentsPerPage = 10;
+
+
+function showPage(pageNumber, list) {
+  studentList.hide();
+
+  for (let i = 0; i <= list.length; i++) {
+    if (i < pageNumber * maxStudentsPerPage && i > (pageNumber - 1) * maxStudentsPerPage - 1)
+      $(list[i]).show();
+    }
+}
+
+function appendPageLinks() {
+  let allPages = Math.ceil(numOfStudents / maxStudentsPerPage);
+  console.log(allPages);
+}
+
+showPage(1, studentList);
+appendPageLinks(studentList);
