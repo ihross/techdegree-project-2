9fa442dd4f83675ba0ae78e7ddacb98445c8755d
Ian Ross <ianhowesross@gmail.com>
2017-09-13 17:12:53 -0400

    elaborating comments

--- pagination-filter-v2/js/script.js
+++ pagination-filter-v2/js/script.js
@@ -1,18 +1,18 @@
-
-const studentList = $(".student-item");
-const numOfStudents = studentList.length;
+// Constant global variables
+const studentList = $(".student-item");   // Accessing the students
+const numOfStudents = studentList.length; // Total students
 const maxStudentsPerPage = 10;
 
-
+// Keeps the students shown on each page limited to 10
 function showPage(pageNumber, list) {
-  studentList.hide();
-
-  for (let i = 0; i <= list.length; i++) {
+  studentList.hide();   // Dynamically hides the complete roster
+  for (let i = 0; i <= list.length; i++) {    // Range of loop stays within the max students (10)
     if (i < pageNumber * maxStudentsPerPage && i > (pageNumber - 1) * maxStudentsPerPage - 1)
-      $(list[i]).show();
+      $(list[i]).show();   // Displays roster on each page within range
     }
 }
 
+// Orders and presents the total pages needed for the full student roster
 function appendPageLinks() {
   let allPages = Math.ceil(numOfStudents / maxStudentsPerPage);
   console.log(allPages);
